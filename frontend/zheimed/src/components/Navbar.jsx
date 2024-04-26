42a0e4bf5d17c97415b1682e3caead2594d0553f
okondev2021 <sojapril16@gmail.com>
2024-04-26 23:26:38 +0100

    frontend: started about us section

--- frontend/zheimed/src/components/Navbar.jsx
+++ frontend/zheimed/src/components/Navbar.jsx
@@ -8,6 +8,7 @@ export const Navbar = () => {
     const navigation = [
         {
             title: 'About Us',
+            
             path: '/'
         },
         {
@@ -16,8 +17,8 @@ export const Navbar = () => {
         }
     ]
     return(
-        <nav className="bg-white border-b md:border-0 mx-10 rounded-lg ">
-          <div className="items-center px-4 max-w-screen-xl mx-auto md:flex md:px-8 justify-between">
+        <nav className="mx-10 bg-white border-b rounded-lg shadow-md md:border-0">
+          <div className="items-center justify-between max-w-screen-xl px-4 mx-auto md:flex md:px-8">
               <div className="flex items-center justify-between py-3 md:py-5 md:block">
                     <a href="javascript:void(0)">
                         <img
@@ -27,17 +28,17 @@ export const Navbar = () => {
                             alt=""
                         />
                     </a>
-                  <div className="md:hidden font-bold">
-                      <button className="text-gray-700 outline-none p-2 rounded-md focus:border-gray-400 focus:border"
+                  <div className="font-bold md:hidden">
+                      <button className="p-2 text-gray-700 rounded-md outline-none focus:border-gray-400 focus:border"
                           onClick={() => setState(!state)}
                       >
                           {
                               state ? (
-                                  <svg xmlns="http://www.w3.org/2000/svg" className="h-6 w-6" viewBox="0 0 20 20" fill="currentColor">
+                                  <svg xmlns="http://www.w3.org/2000/svg" className="w-6 h-6" viewBox="0 0 20 20" fill="currentColor">
                                       <path fillRule="evenodd" d="M4.293 4.293a1 1 0 011.414 0L10 8.586l4.293-4.293a1 1 0 111.414 1.414L11.414 10l4.293 4.293a1 1 0 01-1.414 1.414L10 11.414l-4.293 4.293a1 1 0 01-1.414-1.414L8.586 10 4.293 5.707a1 1 0 010-1.414z" clipRule="evenodd" />
                                   </svg>
                               ) : (
-                                  <svg xmlns="http://www.w3.org/2000/svg" className="h-6 w-6" fill="none" viewBox="0 0 24 24" stroke="currentColor">
+                                  <svg xmlns="http://www.w3.org/2000/svg" className="w-6 h-6" fill="none" viewBox="0 0 24 24" stroke="currentColor">
                                       <path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M4 8h16M4 16h16" />
                                   </svg>
                               )
@@ -46,11 +47,11 @@ export const Navbar = () => {
                   </div>
               </div>
               <div className={`pb-3 mt-8 md:block md:pb-0 md:mt-0 ${ state ? 'block' : 'hidden'}`}>
-                  <ul className="justify-center items-center space-y-8 md:flex md:space-x-6 md:space-y-0">
+                  <ul className="items-center justify-center space-y-8 md:flex md:space-x-6 md:space-y-0">
                       {
                           navigation.map((item, idx) => {
                               return (
-                                <li key={idx} className="text-gray-600 hover:text-indigo-600 font-bold">
+                                <li key={idx} className="font-bold text-gray-600 hover:text-indigo-600">
                                     <a href={item.path}>
                                         { item.title }
                                     </a>
--- frontend/zheimed/src/pages/Landingpage.jsx
+++ frontend/zheimed/src/pages/Landingpage.jsx
@@ -1,10 +1,12 @@
 import { Navbar } from "../components/Navbar"
 import { Hero } from "../components/Hero"
+import { AboutSection } from "../components/AboutSection"
 export const LandingPage = () => {
     return (
         <>
         <div>
             <Hero/>
+            <AboutSection />
             
         </div>
         </>
