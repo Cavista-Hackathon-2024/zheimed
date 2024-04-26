e108bae1b3924d9dc58f4c5fc57a7b68c9c25e36
okondev2021 <sojapril16@gmail.com>
2024-04-26 23:38:10 +0100

    frontend: added about with others

--- frontend/zheimed/src/components/Hero.jsx
+++ frontend/zheimed/src/components/Hero.jsx
@@ -4,24 +4,24 @@ import HeroImg3 from '../assets/Heroimg3.svg'
 import { Navbar } from './Navbar'
 export const Hero = () => {
     return (
-        <section className='relative z-10 bg-black'>
+        <section className='bg-[#1B1818] relative z-10'>
             <div className='absolute w-full h-full wavyBg '></div>
             <section className='fixed w-full pt-6 '>
                 <Navbar />
             </section>
-            <div className="items-center justify-between max-w-screen-xl min-h-screen gap-12 px-4 py-32 mx-auto overflow-hidden text-gray-600 md:px-8 md:flex ">
+            <div className="items-center justify-between max-w-screen-xl min-h-screen gap-32 px-4 py-32 mx-auto overflow-hidden text-gray-600 md:px-8 md:flex ">
                 <div className='flex-none max-w-xl space-y-5'>
                     
                     <h1 className="text-4xl font-extrabold text-white sm:text-5xl">
-                        AI Model that diagnoses Alzheimer disease
+                        An AI Model that detects <span className='text-[#389E5A]'>Alzheimer</span> disease
                     </h1>
                     <p className='text-white'>
                         An AI powered model that utilizes advanced machine learning techniques to analyze MRI scans and identify early signs of Alzheimer's with high accuracy.
                     </p>
                     <button
-                        className="w-1/2 px-6 py-3.5 text-white bg-[#389E5A] rounded-lg duration-150 hover:bg-[#389E5A] active:shadow-lg"
+                        className="w-1/2 px-6 py-3.5 text-white bg-[#389E5A] font-semibold rounded-lg duration-150 hover:bg-[#389E5A] active:shadow-lg"
                     >
-                       Try Demo
+                        Try demo
                     </button>
                 </div>
                 <div className='flex-1 hidden md:block' >
